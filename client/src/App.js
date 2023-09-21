c2893ae9b3f1fb3638e207998aa00786223c99a1
Daniel Ramos <denaelbot@gmail.com>
2023-09-21 12:25:30 -0300

    second commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-
-import './App.css';
-import Properties from './components/Properties';
-import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
-import CssBaseline from '@mui/material/CssBaseline';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import MuiAppBar from '@mui/material/AppBar';
+import "./App.css";
+import Properties from "./components/Properties";
+import { styled, createTheme, ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import MuiAppBar from "@mui/material/AppBar";
 
 const mdTheme = createTheme();
 
 const AppBar = styled(MuiAppBar, {
-  shouldForwardProp: (prop) => prop !== 'open',
+  shouldForwardProp: (prop) => prop !== "open",
 })(({ theme, open }) => ({
-  transition: theme.transitions.create(['width', 'margin'], {
+  transition: theme.transitions.create(["width", "margin"], {
     easing: theme.transitions.easing.sharp,
     duration: theme.transitions.duration.leavingScreen,
   }),
   ...(open && {
-    transition: theme.transitions.create(['width', 'margin'], {
+    transition: theme.transitions.create(["width", "margin"], {
       easing: theme.transitions.easing.sharp,
       duration: theme.transitions.duration.enteringScreen,
     }),
@@ -27,14 +26,16 @@ const AppBar = styled(MuiAppBar, {
 function App() {
   return (
     <ThemeProvider theme={mdTheme}>
-      <Box sx={{ display: 'flex' }}>
+      <Box sx={{ display: "flex" }}>
         <CssBaseline />
         <AppBar position="absolute">
           <Toolbar
             sx={{
-              pr: '24px', // keep right padding when drawer closed
+              pr: "24px", // keep right padding when drawer closed
             }}
-          ><h1>Cadastro de imóveis</h1></Toolbar>
+          >
+            <h1>Cadastro de imóveis</h1>
+          </Toolbar>
         </AppBar>
         <Properties />
       </Box>
